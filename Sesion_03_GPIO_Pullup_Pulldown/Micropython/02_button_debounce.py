922b4b3d6d744db33ff1c4ed7e7b498fd39482fd
LeslieSanchez0 <201831@iberopuebla.mx>
2026-09-11 16:08:25 -0600

    Add button debounce functionality
    Implement button debounce logic to prevent false clicks.

--- Sesion_03_GPIO_Pullup_Pulldown/Micropython/02_button_debounce.py
+++ Sesion_03_GPIO_Pullup_Pulldown/Micropython/02_button_debounce.py
@@ -1,1 +1,17 @@
+from machine import Pin
+from time import sleep_ms
 
+button=Pin(16, Pin.IN, Pin.PULL_UP)
+last=1
+print("Presiona el botón para registrar")
+
+while True:
+    now=button.value()
+    if last==1 and now==0:
+        sleep_ms(30)
+        if button.value()==0:
+            print("CLICK válido")
+            while button.value()==0:
+                sleep_ms(10)
+    last=now
+    sleep_ms(10)
